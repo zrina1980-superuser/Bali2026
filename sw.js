41defc8d3f8edfe0de00289c462e394186bac819
Zrina Grozni <zrina1980@gmail.com>
2026-04-21 23:37:13 +0200

    update 2

--- sw.js
+++ sw.js
@@ -1,26 +1,36 @@
-const CACHE_NAME = 'bali-2026-v2';
-const urlsToCache = [
+// OVO JE NAJVAŽNIJE: Kad god mijenjaš index.html, ovdje samo promijeni V1 u V2, V3 itd.
+const CACHE_NAME = 'bali-2026-v4'; 
+const ASSETS = [
   './',
-  './index.html' // Pazi da se tvoj glavni HTML file zove ovako. Ako se zove drugačije, preimenuj ovo ovdje.
+  './index.html'
 ];
 
-// Instalacija (sprema fileove u cache mobitela)
+// Instalacija novog Workera
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
   );
+  // Odmah gura novu verziju naprijed
+  self.skipWaiting();
 });
 
-// Vuče podatke iz cachea kad nema neta
+// Čišćenje starog smeća
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Vadi iz cachea samo ako nema interneta
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Ako postoji u cacheu, vrati ga, inače probaj skinuti s weba
-        return response || fetch(event.request);
-      })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
